0a98cd854464e4c6b64d13a0ab7d2b75dd7ad6ed
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2018-01-11 15:32:58 -0300

    Add crowdsale contract from OZ

--- truffle.js
+++ truffle.js
@@ -1,1 +1,16 @@
-module.exports = {};
+require('babel-register');
+require('babel-polyfill');
+module.exports = {
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*',
+    },
+    testrpc: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*',
+    },
+  }
+};
