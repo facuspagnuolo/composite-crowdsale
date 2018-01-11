3f77c089ec1c307af4b7b5f1b2584675d2729d77
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2018-01-11 16:34:27 -0300

    Add more crowdsales from OZ

---
+++ contracts/CappedCrowdsale.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.4.18;
+
+import './Crowdsale.sol';
+import 'zeppelin-solidity/contracts/math/SafeMath.sol';
+
+/**
+ * @title CappedCrowdsale
+ * @dev Extension of Crowdsale with a max amount of funds raised
+ */
+contract CappedCrowdsale is Crowdsale {
+  using SafeMath for uint256;
+
+  uint256 public cap;
+
+  function CappedCrowdsale(uint256 _cap) public {
+    require(_cap > 0);
+    cap = _cap;
+  }
+
+  // overriding Crowdsale#validPurchase to add extra cap logic
+  // @return true if investors can buy at the moment
+  function validPurchase(address beneficiary, uint256 value) internal view returns (bool) {
+    bool withinCap = weiRaised.add(value) <= cap;
+    return super.validPurchase(beneficiary, value) && withinCap;
+  }
+
+  // overriding Crowdsale#hasEnded to add cap logic
+  // @return true if crowdsale event has ended
+  function hasEnded() public view returns (bool) {
+    bool capReached = weiRaised >= cap;
+    return super.hasEnded() || capReached;
+  }
+
+}
